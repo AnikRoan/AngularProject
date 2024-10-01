1ae0872a98e22217da51d993137603cf4665e176
Anna Padun <roananik1988@gmail.com>
2024-10-01 14:03:21 +0200

    bugfix-15/-add takeUntilDestroyed(this.destroyRef)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnDestroy, OnInit,} from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Component, DestroyRef, OnInit,} from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { TaskDashboardComponent } from './task-dashboard/task-dashboard.component';
 import { ErrorPageComponent } from './error/error-page/error-page.component';
-import { Subject} from 'rxjs';
-
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
+import { inject } from '@angular/core';
+import { filter } from 'rxjs/operators';
 
 
 
@@ -21,22 +22,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
   title = 'task_tracker';
-  private unsubscribe$ = new Subject<void>();
+ 
   isErrorPage: boolean = false;
+  private destroyRef = inject(DestroyRef);
 
   constructor(private router: Router) {}
   ngOnInit(): void {
-     this.router.events.subscribe((event) => {
+    this.subscribeToErrorEvent();
+  }
+
+  subscribeToErrorEvent(): void {
+    this.router.events
+    .pipe(takeUntilDestroyed(this.destroyRef),
+          filter(event => event instanceof NavigationEnd))
+    
+    .subscribe((event) => {
       this.isErrorPage = this.router.url === '/error';
       console.log("Current isErrorPage status:", this.isErrorPage);
+      
     });
   }
 
-  ngOnDestroy(): void {
-     this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
+ 
 
 }
